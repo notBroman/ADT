6952e479d1e874072ea0beffce64f62680099370
notBroman <notbroman@gmail.com>
2021-03-27 23:01:49 +0100

    added traversal methods

--- ADT/tree_base.py
+++ ADT/tree_base.py
@@ -1,29 +1,38 @@
 import re
 
 class _TreeNode():
-    __slots__ = 'left_child', 'right_child', 'data', 'parent', 'pos'
+    __slots__ = 'left_child', 'right_child', 'data', 'parent'
 
-    def __init__(self, new_data=None, new_parent=None:_TreeNode, new_r_child=None:_TreeNode, new_l_child=None:_TreeNode):
+    def __init__(self, new_data=None):
         self.data = new_data
         self.parent = new_parent
         self.left_child = new_l_child
         self.right_child = new_r_child
-        self.pos = None
 
     def __str__(self):
         return str(self._data)
 
 class Binary_Tree():
-    def __init__(self, new_val=None):
+    def __init__(self, new_val):
         self._root = _TreeNode(new_val)
-        self._size = 0
 
-    def __len__(self):
-        return self._size
+    def preorder_trav(self, start:_TreeNode, trav:str):
+        """traverses root->left->right"""
+        if start:
+            trav += str(start.data) + '->'
+            preorder_trav(self, start.left_child, trav)
+            preorder_trav(self, start.right_child, trav)
+        return trav
 
-    def __iter__(self):
-        TODO
-        # traversal method
+    def inorder_trav(self, start:_TreeNode, trav:str):
+        """traverses left->root->right"""
+        if start:
+            inorder_trav(self, start.left_child, trav)
+            trav += str(start.data) + '->'
+            inorder_trav(self, start.right_child, trav)
+
+    def postorder_trav(self, start:_TreeNode, trav:str):
+        """traverses left->right->root"""
 
     def get_root(self):
         return self._root
@@ -31,9 +40,14 @@ class Binary_Tree():
     def add_left(self, val):
         if self._size ==  0:
             self._root.left_child = _TreeNode(val, self._root)
+        else:
+            TODO
 
     def add_right(self, val):
-        TODO
+        if self._size == 0:
+            self._root.right_child = _TreeNode(val, self._root)
+        else:
+            TODO
 
     def rm(self):
         TODO
