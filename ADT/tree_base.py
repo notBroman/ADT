ecb27d09cc76a0a1ea4aa953252b018e6f5f2da3
notBroman <notbroman@gmail.com>
2021-03-28 18:25:48 +0200

    added destructor and adding of children

--- ADT/tree_base.py
+++ ADT/tree_base.py
@@ -12,6 +12,55 @@ class _TreeNode():
     def __str__(self):
         return str(self._data)
 
+    def __del__(self):
+        """ DO NOT USE INCOMPLETE/INCORRECT CODE """
+        if self.is_root():
+            # mistake in structruing need to check for children always
+            # dont check for parent if node is root
+            pass
+        else:
+            """ Node has right and left child """
+            if self.left_child is not None and self.right_child is not None:
+                if self is self.parent.left_child:
+                    # implement logic to replace self correctly
+                elif self is self.parent.right_child:
+                    # implement logic to replace self correctly
+
+            """ Node only has left child """
+            elif self.left_child is not None and self.right_child is None:
+                # node is parents left child
+                if self is self.parent.left_child:
+                    self.left_child.parent = self.parent
+                    self.parent.left_child = self.left_child
+                # node is parents right child
+                elif self is self.parent.right_child:
+                    self.left_child.parent = self.parent
+                    self.parent.right_child = self.left_child
+                # in case something goes wrong
+                else:
+                    print("case not accounted for")
+                    break
+
+            """ Node only has right child """
+            elif self.left_child is None and self.right_child is not None:
+                # node is parents left child
+                if self is self.parent.left_child:
+                    self.right_child.parent = self.parent
+                    self.parent.left_child = self.right_child
+                # node is parents right child
+                elif self is self.parent.right_child:
+                    self.right_child.parent = self.parent
+                    self.parent.right_child = self.right_child
+                # in case something goes wrong
+                else:
+                    print("case not accounted for")
+                    break
+
+        self.data = None
+        self.parent = None
+        self.left_child = None
+        self.right_child = None
+
 class Binary_Tree():
     def __init__(self, new_val):
         self._root = _TreeNode(new_val)
@@ -46,22 +95,25 @@ class Binary_Tree():
     def get_root(self):
         return self._root
 
-    def add_left(self, val):
-        if self._size ==  0:
-            self._root.left_child = _TreeNode(val, self._root)
+    def add_left(self, val, parent:_TreeNode):
+        """ return 1 -> success | return 0 -> failure """
+        if type(parent) is _TreeNode:
+            parent.left_child = _TreeNode(val)
+            parent.left_child.parent = parent
+            return 1
         else:
-            TODO
+            return 0
 
-    def add_right(self, val):
-        if self._size == 0:
-            self._root.right_child = _TreeNode(val, self._root)
+    def add_right(self, val, parent:_TreeNode):
+        """ return 1 -> success | return 0 -> failure"""
+        if type(parent) is _TreeNode:
+            parent.right_child = _TreeNode(val)
+            parent.right_child.parent = parent
+            return 1
         else:
-            TODO
+            return 0
 
     def rm(self):
-        TODO
-
-    def replace(self, position):
         TODO
 
 if __name__ == '__main__':
